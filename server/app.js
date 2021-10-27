e31cdbe1bf3c823da91eacd378a2fed43a3c92fa
Lisa Doan <ldoan24@gmail.com>
2021-10-27 12:59:08 -0700

    Completed node101-movie-finder-data

--- server/app.js
+++ server/app.js
@@ -1,8 +1,31 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const axios = require("axios");
 
-
-const app = express();
+let app = express();
 
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+
+app.use(morgan("dev"));
+
+let cache = {};
+
+app.use(morgan("dev"));
+
+app.get("/", function (req, res) {
+  if (!cache[req.url]) {
+    axios
+      .get("http://www.omdbapi.com" + req.url + "&apikey=8730e0e")
+      .then(function (response) {
+        cache[req.url] = response.data;
+        res.json(response.data);
+      })
+      .catch(function (error) {
+        res.status(500).send({ error: "Internal Server Error" });
+      });
+  } else {
+    res.json(cache[req.url]);
+  }
+});
 
 module.exports = app;
